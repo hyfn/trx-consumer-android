ec32e8923e7fc2e675fb1487c1fc307b923ba294
Vivek Vishwanath <vivekv210@gmail.com>
2021-03-30 17:03:15 -0500

    WIP

---
+++ app/src/main/java/com/trx/consumer/screens/testutility/TestUtilityFragment.kt
@@ -0,0 +1,6 @@
+package com.trx.consumer.screens.testutility
+
+import com.trx.consumer.base.BaseFragment
+
+class TestUtilityFragment: BaseFragment() {
+}
---
+++ app/src/main/java/com/trx/consumer/screens/testutility/TestUtilityViewModel.kt
@@ -0,0 +1,6 @@
+package com.trx.consumer.screens.testutility
+
+import com.trx.consumer.base.BaseViewModel
+
+class TestUtilityViewModel: BaseViewModel() {
+}
