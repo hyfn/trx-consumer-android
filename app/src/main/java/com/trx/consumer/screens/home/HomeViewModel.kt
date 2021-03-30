7808f34df2aa721c98b8d1eb524ae2944cd2aadc
Vivek Vishwanath <vivekv210@gmail.com>
2021-03-30 17:42:32 -0500

    Set up `TestUtility`

--- app/src/main/java/com/trx/consumer/screens/home/HomeViewModel.kt
+++ app/src/main/java/com/trx/consumer/screens/home/HomeViewModel.kt
@@ -1,5 +1,21 @@
 package com.trx.consumer.screens.home
 
 import com.trx.consumer.base.BaseViewModel
+import com.trx.consumer.common.CommonLiveEvent
 
-class HomeViewModel : BaseViewModel()
+class HomeViewModel : BaseViewModel() {
+
+    //region Events
+
+    val eventTapTest = CommonLiveEvent<Void>()
+
+    //endregion
+
+    //region Functions
+
+    fun doTapTest() {
+        eventTapTest.call()
+    }
+
+    //endregion
+}
--- app/src/main/java/com/trx/consumer/screens/testutility/TestUtilityViewModel.kt
+++ app/src/main/java/com/trx/consumer/screens/testutility/TestUtilityViewModel.kt
@@ -1,6 +1,21 @@
 package com.trx.consumer.screens.testutility
 
 import com.trx.consumer.base.BaseViewModel
+import com.trx.consumer.common.CommonLiveEvent
 
-class TestUtilityViewModel: BaseViewModel() {
+class TestUtilityViewModel : BaseViewModel() {
+
+    //region Events
+
+    val eventTapBack = CommonLiveEvent<Void>()
+
+    //endregion
+
+    //region Functions
+
+    fun doTapBack() {
+        eventTapBack.call()
+    }
+
+    //endregion
 }
