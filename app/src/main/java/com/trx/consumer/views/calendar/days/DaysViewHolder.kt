353da999bdac9974cf400aa356b6fa163738a4bc
Halil Yeşilyurt <yesilyurthalil@hotmail.com>
2021-04-06 18:02:10 +0300

    added calendar to test utility. and some fixes.

--- app/src/main/java/com/trx/consumer/views/calendar/days/DaysViewHolder.kt
+++ app/src/main/java/com/trx/consumer/views/calendar/days/DaysViewHolder.kt
@@ -2,16 +2,13 @@ package com.trx.consumer.views.calendar.days
 
 import android.view.View
 import com.trx.consumer.R
-import com.trx.consumer.common.CommonImageView
 import com.trx.consumer.common.CommonLabel
 import com.trx.consumer.common.CommonView
 import com.trx.consumer.common.CommonViewHolder
-import com.trx.consumer.extensions.setTint
 import com.trx.consumer.models.common.DaysModel
 
 class DaysViewHolder(view: View) : CommonViewHolder(view) {
 
-    private val imgDot: CommonImageView = view.findViewById(R.id.imgDot)
     private val lblTop: CommonLabel = view.findViewById(R.id.lblTop)
     private val lblBottom: CommonLabel = view.findViewById(R.id.lblBottom)
     private val viewDays: CommonView = view.findViewById(R.id.viewDays)
@@ -21,16 +18,13 @@ class DaysViewHolder(view: View) : CommonViewHolder(view) {
         updateListener: DaysUpdateListener?,
         tapListener: DaysTapListener?
     ) {
-        imgDot.setTint(model.state.dotColor)
 
         lblTop.apply {
             text(model.weekDayInitial)
-            textColor(model.state.textColor)
         }
 
         lblBottom.apply {
             text(model.dayNumber)
-            textColor(model.state.textColor)
             setBackgroundResource(model.state.bgDrawable)
         }
 
