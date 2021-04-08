0a052a0ff66208b5dce5cd16b834563c2e0abd70
Halil Yeşilyurt <yesilyurthalil@hotmail.com>
2021-04-08 20:00:36 +0300

    removed unused code blocks.

--- app/src/main/java/com/trx/consumer/models/common/PlansListModel.kt
+++ app/src/main/java/com/trx/consumer/models/common/PlansListModel.kt
@@ -1,17 +1,11 @@
 package com.trx.consumer.models.common
 
-import android.content.Context
 import android.os.Parcelable
-import android.text.SpannableStringBuilder
-import androidx.core.content.ContextCompat
-import androidx.core.text.color
 import com.trx.consumer.BuildConfig
-import com.trx.consumer.R
 import com.trx.consumer.screens.plans.list.PlansListViewState
 import kotlinx.parcelize.Parcelize
 import org.json.JSONObject
 import java.text.NumberFormat
-import java.util.Date
 import java.util.Locale
 
 @Parcelize
@@ -24,27 +18,10 @@ class PlansListModel(
     var isRecurring: Boolean = false,
     var packageId: String = "",
     var price: Double = 0.0,
-    var renewsAtDate: Date? = null,
     var contractUrl: String = ""
 ) : Parcelable {
 
-    private val days: Int
-        get() {
-            renewsAtDate?.let {
-                // TODO: Insert proper logic
-
-                //  return date.daysUntil
-                return 21
-            } ?: run {
-                return 0
-            }
-            return 21
-        }
-
     var state: PlansListViewState = PlansListViewState.UNSELECTED
-
-    val creditsText: String
-        get() = "$credits Credit${if (credits == 1) "" else "s"}"
 
     private val intervalPlural: String
         get() =
@@ -70,29 +47,6 @@ class PlansListModel(
             return if (isRecurring) "$formattedPrice/$intervalText" else formattedPrice
         }
 
-    private val renewGenericText: SpannableStringBuilder
-        get() {
-            return if (isRecurring) {
-                if (interval == "month") {
-                    SpannableStringBuilder("Renews monthly")
-                } else {
-                    SpannableStringBuilder("Renews every $intervalCount $intervalPlural")
-                }
-            } else {
-                SpannableStringBuilder("One-Time Purchase")
-            }
-        }
-
-    private fun renewInText(context: Context): SpannableStringBuilder {
-        return SpannableStringBuilder("Renews in ")
-            .color(
-                ContextCompat.getColor(
-                    context,
-                    R.color.blue
-                )
-            ) { append("$days days") }
-    }
-
     companion object {
 
         fun parse(jsonObject: JSONObject): PlansListModel {
@@ -114,7 +68,7 @@ class PlansListModel(
                 credits = 50
                 price = 49.0
                 description = "Typically covers 4-6 classes."
-                contractUrl = BuildConfig.kWaiversUrl
+                contractUrl = BuildConfig.trxUrl
             }
         }
 
