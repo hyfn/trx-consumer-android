0ee73421b428adf5b2749700e6758ad2e580063b
Halil Yeşilyurt <yesilyurthalil@hotmail.com>
2021-04-23 17:27:38 +0300

    added dummy test plan list.

--- app/src/main/java/com/trx/consumer/screens/plans/PlansViewModel.kt
+++ app/src/main/java/com/trx/consumer/screens/plans/PlansViewModel.kt
@@ -2,12 +2,22 @@ package com.trx.consumer.screens.plans
 
 import com.trx.consumer.base.BaseViewModel
 import com.trx.consumer.common.CommonLiveEvent
+import com.trx.consumer.models.common.PlanModel
+import com.trx.consumer.screens.plans.list.PlansListener
 
-class PlansViewModel : BaseViewModel() {
+class PlansViewModel : BaseViewModel(), PlansListener {
 
+    val eventLoadView = CommonLiveEvent<List<PlanModel>>()
     val eventTapBack = CommonLiveEvent<Void>()
+
+    fun doLoadView() {
+        eventLoadView.postValue(PlanModel.testList(5))
+    }
 
     fun doTapBack() {
         eventTapBack.call()
+    }
+
+    override fun doTapPlan(model: PlanModel) {
     }
 }
