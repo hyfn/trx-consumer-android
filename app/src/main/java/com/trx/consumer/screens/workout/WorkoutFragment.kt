727c53276d1a84451212b72e7cabcd19afc1fd29
Halil Yeşilyurt <yesilyurthalil@hotmail.com>
2021-04-30 13:39:35 +0300

    completed.

--- app/src/main/java/com/trx/consumer/screens/workout/WorkoutFragment.kt
+++ app/src/main/java/com/trx/consumer/screens/workout/WorkoutFragment.kt
@@ -1,13 +1,53 @@
 package com.trx.consumer.screens.workout
 
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import com.trx.consumer.R
 import com.trx.consumer.base.BaseFragment
 import com.trx.consumer.base.viewBinding
 import com.trx.consumer.databinding.FragmentWorkoutBinding
+import com.trx.consumer.managers.NavigationManager
+import com.trx.consumer.models.common.BookingViewState
 
 class WorkoutFragment : BaseFragment(R.layout.fragment_workout) {
 
     private val viewModel: WorkoutViewModel by viewModels()
     private val viewBinding by viewBinding(FragmentWorkoutBinding::bind)
+
+    override fun bind() {
+        viewModel.apply {
+            eventTapBack.observe(viewLifecycleOwner, handleTapBack)
+            eventLoadView.observe(viewLifecycleOwner, handleLoadView)
+            doLoadView()
+        }
+
+        viewBinding.btnBack.setOnClickListener { viewModel.doTapBack() }
+    }
+
+    private val handleTapBack = Observer<Void> {
+        NavigationManager.shared.dismiss(this)
+    }
+
+    private val handleLoadView = Observer<BookingViewState> {
+        viewBinding.apply {
+            btnPrimary.setBackgroundColor(
+                ContextCompat.getColor(
+                    requireContext(),
+                    it.buttonBackgroundColor
+                )
+            )
+
+            btnPrimary.setTextColor(
+                ContextCompat.getColor(
+                    requireContext(),
+                    it.buttonTitleColor
+                )
+            )
+        }
+    }
+
+    override fun onBackPressed() {
+        viewModel.doTapBack()
+    }
 }
--- app/src/main/java/com/trx/consumer/screens/workout/WorkoutViewModel.kt
+++ app/src/main/java/com/trx/consumer/screens/workout/WorkoutViewModel.kt
@@ -1,5 +1,21 @@
 package com.trx.consumer.screens.workout
 
 import com.trx.consumer.base.BaseViewModel
+import com.trx.consumer.common.CommonLiveEvent
+import com.trx.consumer.models.common.BookingViewState
 
-class WorkoutViewModel : BaseViewModel()
+class WorkoutViewModel : BaseViewModel() {
+
+    var state: BookingViewState = BookingViewState.CANCEL
+
+    val eventLoadView = CommonLiveEvent<BookingViewState>()
+    val eventTapBack = CommonLiveEvent<Void>()
+
+    fun doTapBack() {
+        eventTapBack.call()
+    }
+
+    fun doLoadView() {
+        eventLoadView.postValue(state)
+    }
+}
