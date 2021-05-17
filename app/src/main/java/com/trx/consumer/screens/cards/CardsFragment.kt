92f74aa102fdffc03305776457aa10190cf157b2
Patrick Martin <patrick@sprintfwd.com>
2021-05-17 13:45:08 -0700

    Initial pass at fragments and viewmodel: add events, handling, etc.

--- app/src/main/java/com/trx/consumer/screens/cards/CardsFragment.kt
+++ app/src/main/java/com/trx/consumer/screens/cards/CardsFragment.kt
@@ -8,46 +8,109 @@ import com.trx.consumer.base.BaseFragment
 import com.trx.consumer.base.viewBinding
 import com.trx.consumer.databinding.FragmentCardsBinding
 import com.trx.consumer.extensions.action
+import com.trx.consumer.managers.LogManager
 import com.trx.consumer.managers.NavigationManager
 import com.trx.consumer.models.common.CardModel
 
 class CardsFragment : BaseFragment(R.layout.fragment_cards) {
 
+    //region Objects
+
     private val viewModel: CardsViewModel by viewModels()
     private val viewBinding by viewBinding(FragmentCardsBinding::bind)
 
-    override fun bind() {
-        viewBinding.btnBack.action { viewModel.doTapBack() }
+    //endregion
 
+    //region Initializers
+
+    override fun bind() {
+        viewBinding.apply {
+            btnBack.action { viewModel.doTapBack() }
+        }
         viewModel.apply {
-            eventLoadView.observe(viewLifecycleOwner, handleLoadView)
+            eventLoadViewSuccess.observe(viewLifecycleOwner, handleLoadViewSuccess)
+            eventLoadViewFailure.observe(viewLifecycleOwner, handleLoadViewFailure)
+            eventTapAdd.observe(viewLifecycleOwner, handleTapAdd)
             eventTapBack.observe(viewLifecycleOwner, handleTapBack)
+            eventTapReplace.observe(viewLifecycleOwner, handleTapReplace)
+            eventShowHud.observe(viewLifecycleOwner, handleShowHud)
             doLoadView()
         }
+    }
+
+    //endregion
+
+    //region Handlers
+
+    private val handleLoadViewSuccess = Observer<CardModel?> {
+        it?.let { model ->
+            viewBinding.apply {
+                viewCard.isVisible = true
+                lblEmpty.isVisible = false
+                ivIcon.setImageResource(model.imageName)
+                lblType.text = lblType.context.getString(model.typeText)
+                lblNumber.text = model.number
+                btnPayment.text = getString(R.string.cards_update_payment_method_label)
+            }
+        } ?: run {
+            viewBinding.apply {
+                viewCard.isVisible = false
+                lblEmpty.isVisible = true
+                btnPayment.text = getString(R.string.cards_add_payment_method_label)
+            }
+        }
+
+        // viewBinding.apply {
+        //     if (it != null) {
+        //         viewCard.isVisible = true
+        //         lblEmpty.isVisible = false
+        //         ivIcon.setImageResource(it.imageName)
+        //         lblType.text = lblType.context.getString(it.typeText)
+        //         lblNumber.text = it.number
+        //         btnPayment.text = getString(R.string.cards_update_payment_method_label)
+        //     } else {
+        //         viewCard.isVisible = false
+        //         lblEmpty.isVisible = true
+        //         btnPayment.text = getString(R.string.cards_add_payment_method_label)
+        //     }
+        // }
+    }
+
+    private val handleLoadViewFailure = Observer<Void> {
+        LogManager.log("handleLoadViewFailure")
+        //  TODO: Add AlertModel
+/*        val model =
+            AlertModel.error("Could not load cards")
+        model.setPrimaryButton(R.string.alert_primary_back) {
+            NavigationManager.shared.dismiss(this, null)
+        }
+        model.setPrimaryButton(R.string.alert_secondary_report) {
+            UtilityManager.shared.showContactSupport()
+        }
+        NavigationManager.shared.present(this, R.id.alert_fragment, model)*/
+    }
+
+    private val handleTapAdd = Observer<Void> {
+        LogManager.log("handleTapAdd")
+        NavigationManager.shared.present(this, R.id.add_card_fragment)
+    }
+
+    private val handleTapBack = Observer<Void> {
+        NavigationManager.shared.dismiss(this)
     }
 
     override fun onBackPressed() {
         viewModel.onBackPressed()
     }
 
-    private val handleLoadView = Observer<CardModel?> {
-        viewBinding.apply {
-            if (it != null) {
-                viewCard.isVisible = true
-                lblEmpty.isVisible = false
-                ivIcon.setImageResource(it.imageName)
-                lblType.text = lblType.context.getString(it.typeText)
-                lblNumber.text = it.number
-                btnPayment.text = getString(R.string.cards_update_payment_method_label)
-            } else {
-                viewCard.isVisible = false
-                lblEmpty.isVisible = true
-                btnPayment.text = getString(R.string.cards_add_payment_method_label)
-            }
-        }
+    private val handleTapReplace = Observer<Void> {
+        LogManager.log("handleTapReplace")
+        NavigationManager.shared.present(this, R.id.add_card_fragment)
     }
 
-    private val handleTapBack = Observer<Void> {
-        NavigationManager.shared.dismiss(this)
+    private val handleShowHud = Observer<Boolean> { show ->
+        viewBinding.hudView.isVisible = show
     }
+
+    //endregion
 }
--- app/src/main/java/com/trx/consumer/screens/cards/CardsViewModel.kt
+++ app/src/main/java/com/trx/consumer/screens/cards/CardsViewModel.kt
@@ -1,19 +1,64 @@
 package com.trx.consumer.screens.cards
 
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.viewModelScope
 import com.trx.consumer.base.BaseViewModel
 import com.trx.consumer.common.CommonLiveEvent
+import com.trx.consumer.managers.BackendManager
+import com.trx.consumer.managers.CacheManager
 import com.trx.consumer.models.common.CardModel
+import kotlinx.coroutines.launch
 
-class CardsViewModel : BaseViewModel() {
+class CardsViewModel @ViewModelInject constructor(
+    private val backendManager: BackendManager,
+    private val cacheManager: CacheManager
+) : BaseViewModel() {
+
+    //region Objects
 
     var model: CardModel? = null
 
-    val eventLoadView = CommonLiveEvent<CardModel?>()
+    //endregion
+
+    //region Events
+
+    val eventLoadViewSuccess = CommonLiveEvent<CardModel?>()
+    val eventLoadViewFailure = CommonLiveEvent<Void>()
+    val eventTapAdd = CommonLiveEvent<Void>()
     val eventTapBack = CommonLiveEvent<Void>()
+    val eventTapReplace = CommonLiveEvent<Void>()
+
+    val eventLoadView = CommonLiveEvent<CardModel?>()
     val eventShowHud = CommonLiveEvent<Boolean>()
 
+    //endregion
+
+    //region Actions
+
     fun doLoadView() {
-        eventLoadView.postValue(CardModel.test())
+        viewModelScope.launch {
+            eventShowHud.postValue(true)
+            val response = backendManager.user()
+            eventShowHud.postValue(false)
+            when {
+                response.isSuccess -> {
+                    cacheManager.user()?.card.let {
+                        model = it
+                        eventLoadViewSuccess.postValue(model)
+                    }
+                }
+                response.statusCode == 404 -> {
+                    eventLoadViewSuccess.postValue(model)
+                }
+                else -> {
+                    eventLoadViewFailure.call()
+                }
+            }
+        }
+    }
+
+    fun doTapAdd() {
+        eventTapAdd.call()
     }
 
     fun doTapBack() {
@@ -23,4 +68,10 @@ class CardsViewModel : BaseViewModel() {
     fun onBackPressed() {
         eventTapBack.call()
     }
+
+    fun doTapReplace() {
+        eventTapReplace.call()
+    }
+
+    //endregion
 }
