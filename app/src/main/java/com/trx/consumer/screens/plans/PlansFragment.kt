68d6b3cf9bab0a3b95af2a0a1a520d8a1e9eceae
Patrick Martin <patrick@sprintfwd.com>
2021-05-19 14:18:05 -0700

    Initial Plans frag/vm commit with events, empty function bodies, regions, handlers, etc.

--- app/src/main/java/com/trx/consumer/screens/plans/PlansFragment.kt
+++ app/src/main/java/com/trx/consumer/screens/plans/PlansFragment.kt
@@ -8,23 +8,25 @@ import com.trx.consumer.base.BaseFragment
 import com.trx.consumer.base.viewBinding
 import com.trx.consumer.databinding.FragmentPlansBinding
 import com.trx.consumer.extensions.action
+import com.trx.consumer.managers.LogManager
 import com.trx.consumer.managers.NavigationManager
 import com.trx.consumer.models.common.PlanModel
 import com.trx.consumer.screens.plans.list.PlansAdapter
 
 class PlansFragment : BaseFragment(R.layout.fragment_plans) {
 
+    //region Objects
+
     private val viewModel: PlansViewModel by viewModels()
     private val viewBinding by viewBinding(FragmentPlansBinding::bind)
 
     private lateinit var adapter: PlansAdapter
 
-    override fun bind() {
-        viewModel.apply {
-            eventLoadView.observe(viewLifecycleOwner, handleLoadView)
-            eventTapBack.observe(viewLifecycleOwner, handleTapBack)
-        }
+    //endregion
 
+    //region Initializers
+
+    override fun bind() {
         viewBinding.apply {
             adapter = PlansAdapter(viewModel) { lifecycleScope }
             rvPlans.adapter = adapter
@@ -32,18 +34,60 @@ class PlansFragment : BaseFragment(R.layout.fragment_plans) {
             btnBack.action { viewModel.doTapBack() }
         }
 
+        viewModel.apply {
+            eventLoadCanCancel.observe(viewLifecycleOwner, handleLoadCanCancel)
+            eventLoadCancelSubscription.observe(viewLifecycleOwner, handleLoadCancelSubscription)
+            eventLoadConfirmSubscription.observe(viewLifecycleOwner, handleLoadConfirmSubscription)
+            eventLoadError.observe(viewLifecycleOwner, handleLoadError)
+            eventLoadNextBillDate.observe(viewLifecycleOwner, handleLoadNextBillDate)
+            eventLoadView.observe(viewLifecycleOwner, handleLoadView)
+            eventLoadPlans.observe(viewLifecycleOwner, handleLoadPlans)
+
+            eventTapBack.observe(viewLifecycleOwner, handleTapBack)
+        }
+
         viewModel.doLoadView()
+    }
+
+    //endregion
+
+    //region Handlers
+
+    private val handleLoadCanCancel = Observer<Boolean> {
+        LogManager.log("handleLoadCanCancel")
+    }
+
+    private val handleLoadCancelSubscription = Observer<String?> {
+        LogManager.log("handleLoadCancelSubscription")
+    }
+
+    private val handleLoadConfirmSubscription = Observer<PlanModel> {
+        LogManager.log("handleLoadConfirmSubscription")
+    }
+
+    private val handleLoadError = Observer<String> {
+        LogManager.log("handleLoadError")
+    }
+
+    private val handleLoadNextBillDate = Observer<String> {
+        LogManager.log("handleLoadNextBillDate")
+    }
+
+    private val handleLoadView = Observer<Void> {
+        LogManager.log("handleLoadView")
+    }
+
+    private val handleLoadPlans = Observer<List<PlanModel>> {
+        LogManager.log("handleLoadPlans")
     }
 
     private val handleTapBack = Observer<Void> {
         NavigationManager.shared.dismiss(this)
     }
 
-    private val handleLoadView = Observer<List<PlanModel>> {
-        adapter.updatePlans(it)
-    }
-
     override fun onBackPressed() {
         viewModel.doTapBack()
     }
+
+    //endregion
 }
--- app/src/main/java/com/trx/consumer/screens/plans/PlansViewModel.kt
+++ app/src/main/java/com/trx/consumer/screens/plans/PlansViewModel.kt
@@ -7,17 +7,41 @@ import com.trx.consumer.screens.plans.list.PlansListener
 
 class PlansViewModel : BaseViewModel(), PlansListener {
 
-    val eventLoadView = CommonLiveEvent<List<PlanModel>>()
+    //region Events
+
+    val eventLoadCanCancel = CommonLiveEvent<Boolean>()
+    val eventLoadCancelSubscription = CommonLiveEvent<String?>()
+    val eventLoadConfirmSubscription = CommonLiveEvent<PlanModel>()
+    val eventLoadError = CommonLiveEvent<String>()
+    val eventLoadNextBillDate = CommonLiveEvent<String>()
+    val eventLoadView = CommonLiveEvent<Void>()
+    val eventLoadPlans = CommonLiveEvent<List<PlanModel>>()
+
     val eventTapBack = CommonLiveEvent<Void>()
 
+    //endregion
+
+    //region Actions
+
     fun doLoadView() {
-        eventLoadView.postValue(PlanModel.testList(5))
+        eventLoadView.call()
+    }
+
+    fun doLoadPlans() {
+    }
+
+    fun doCallSubscribe() {
     }
 
     fun doTapBack() {
         eventTapBack.call()
     }
 
+    fun doTapUnSubscribe() {
+    }
+
     override fun doTapPlan(model: PlanModel) {
     }
+
+    //endregion
 }
