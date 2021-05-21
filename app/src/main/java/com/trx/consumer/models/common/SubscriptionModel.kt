e4a552eefad160e98db81f3208db2291ee62343c
Patrick Martin <patrick@sprintfwd.com>
2021-05-21 00:29:12 -0700

    Use toPrice extension in priceDisplay and plan gettter in SubscriptionModel

--- app/src/main/java/com/trx/consumer/models/common/SubscriptionModel.kt
+++ app/src/main/java/com/trx/consumer/models/common/SubscriptionModel.kt
@@ -1,6 +1,7 @@
 package com.trx.consumer.models.common
 
 import com.trx.consumer.extensions.map
+import com.trx.consumer.extensions.toPrice
 import org.json.JSONObject
 
 class SubscriptionModel(
@@ -9,6 +10,17 @@ class SubscriptionModel(
     var title: String = "",
     var valueProps: List<String> = listOf()
 ) {
+
+    val priceDisplay: String
+        get() = price.toPrice()
+
+    val plan: PlanModel
+        get() = PlanModel(
+            key = key,
+            title = title,
+            cost = priceDisplay,
+            description = valueProps.joinToString(separator = "\n")
+        )
 
     companion object {
 
