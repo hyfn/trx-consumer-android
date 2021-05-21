75d235372cce69832654d294b46db5c8b9eef095
Patrick Martin <patrick@sprintfwd.com>
2021-05-21 00:18:44 -0700

    Add plans function and parse function that will return a PlansResponseModel

--- app/src/main/java/com/trx/consumer/models/responses/PlansResponseModel.kt
+++ app/src/main/java/com/trx/consumer/models/responses/PlansResponseModel.kt
@@ -1,17 +1,37 @@
 package com.trx.consumer.models.responses
 
+import com.trx.consumer.models.common.PlanModel
 import com.trx.consumer.models.common.SubscriptionsModel
+import com.trx.consumer.screens.plans.list.PlansViewState
 import org.json.JSONObject
 
-class PlansResponseModel(val subscription: SubscriptionsModel) {
+class PlansResponseModel(private var subscription: SubscriptionsModel = SubscriptionsModel()) {
+
+    fun plans(subscriptionText: String?): List<PlanModel> {
+        return subscriptionText?.let { text ->
+            val list = subscription.plans.map { it.plan }.toMutableList()
+            list.indexOfFirst { it.title == text }.let { index ->
+                if (index != -1) {
+                    list.removeAt(index).let { planModel ->
+                        planModel.primaryState = PlansViewState.CURRENT
+                        list.add(0, planModel)
+                    }
+                }
+            }
+            list
+        } ?: subscription.plans.map { it.plan }
+    }
 
     companion object {
 
-        fun parse(json: String): SubscriptionsModel {
-            val jsonObject = JSONObject(json)
-                .getJSONObject("data")
-                .getJSONObject("customer")
-            return SubscriptionsModel.parse(jsonObject)
+        fun parse(json: String): PlansResponseModel {
+            return PlansResponseModel(
+                subscription = JSONObject(json)
+                    .getJSONObject("data")
+                    .getJSONObject("customer").let {
+                        SubscriptionsModel.parse(it)
+                    }
+            )
         }
     }
 }
