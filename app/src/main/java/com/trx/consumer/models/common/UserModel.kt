c0716ea928f23298cc982c0b3efe9db5280283ff
Patrick Martin <patrick@sprintfwd.com>
2021-05-21 00:21:14 -0700

    Add subscription specific getters in UserModel, add TODOS for better logic and to remove experiments.

--- app/src/main/java/com/trx/consumer/models/common/UserModel.kt
+++ app/src/main/java/com/trx/consumer/models/common/UserModel.kt
@@ -1,5 +1,6 @@
 package com.trx.consumer.models.common
 
+import com.trx.consumer.managers.LogManager
 import org.json.JSONObject
 
 class UserModel(
@@ -8,6 +9,7 @@ class UserModel(
     val cardPaymentMethod: String? = null,
     var email: String = "",
     var password: String = "",
+    var subscriptions: HashMap<String, UserSubscriptionModel> = hashMapOf(),
     var zipCode: String = "",
     var firstName: String = "",
     var lastName: String = "",
@@ -16,16 +18,61 @@ class UserModel(
     val fullName: String
         get() = "$firstName $lastName"
 
+    //  TODO: Add correct logic
+    val subscription: String?
+        get() = "Test"
+
+    val subscriptionIsCancelled: Boolean
+        get() {
+            return subscriptions.keys.firstOrNull()?.let { key ->
+                subscriptions[key]?.cancelAtPeriodEnd ?: false
+            } ?: false
+        }
+
+    //  TODO: Add correct logic
+    val subscriptionRenewsDateDisplay: String?
+        get() = "Test"
+
+    val subscriptionText: String
+        get() = if (!subscriptionIsCancelled && subscriptions.keys.contains("UNLIMITED")) {
+            kSubscriptionNameUnlimited
+        } else kSubscriptionNamePay
+
     companion object {
 
+        const val kSubscriptionNamePay = "Pay As You Go"
+        const val kSubscriptionNameUnlimited = "Unlimited LIVE Classes"
+
         fun parse(jsonObject: JSONObject): UserModel {
+
             return UserModel(
                 birthday = jsonObject.optString("birthday"),
                 email = jsonObject.optString("email"),
                 zipCode = jsonObject.optString("postalCode"),
                 firstName = jsonObject.optString("firstName"),
                 lastName = jsonObject.optString("lastName")
-            )
+            ).apply {
+                try {
+                    jsonObject.getJSONObject("subscriptions").let { subsJSONObject ->
+                        //  TODO: Remove after testing.
+                        // val keys = subsJSONObject.keys()
+                        // while (keys.hasNext()) {
+                        //     val key = keys.next()
+                        //     val userSubsJSONObject = subsJSONObject.get(key) as JSONObject
+                        //     val userSubs = UserSubscriptionModel.parse(userSubsJSONObject)
+                        //     subscriptions[key] = userSubs
+                        // }
+
+                        subsJSONObject.keys().forEach { key ->
+                            val userSubsJSONObject = subsJSONObject.get(key) as JSONObject
+                            val userSubs = UserSubscriptionModel.parse(userSubsJSONObject)
+                            subscriptions[key] = userSubs
+                        }
+                    }
+                } catch (e: Exception) {
+                    LogManager.log(e)
+                }
+            }
         }
 
         fun test(): UserModel {
