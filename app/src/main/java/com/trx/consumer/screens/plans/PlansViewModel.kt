784b2435446d4d7ca07669c1f36e4de6e1ce9cd8
Patrick Martin <patrick@sprintfwd.com>
2021-05-21 00:16:53 -0700

    Add userModel object, eventShowHud, function bodies for doLoadPlans, doCallSubscribe, doTapUnsubscribe, doTapChoosePlan.

--- app/src/main/java/com/trx/consumer/screens/plans/PlansViewModel.kt
+++ app/src/main/java/com/trx/consumer/screens/plans/PlansViewModel.kt
@@ -6,7 +6,10 @@ import com.trx.consumer.base.BaseViewModel
 import com.trx.consumer.common.CommonLiveEvent
 import com.trx.consumer.managers.BackendManager
 import com.trx.consumer.managers.CacheManager
+import com.trx.consumer.managers.LogManager
 import com.trx.consumer.models.common.PlanModel
+import com.trx.consumer.models.common.UserModel
+import com.trx.consumer.models.responses.PlansResponseModel
 import com.trx.consumer.screens.plans.list.PlansListener
 import kotlinx.coroutines.launch
 
@@ -14,6 +17,12 @@ class PlansViewModel @ViewModelInject constructor(
     private val backendManager: BackendManager,
     private val cacheManager: CacheManager
 ) : BaseViewModel(), PlansListener {
+
+    //region Objects
+
+    private var userModel: UserModel? = null
+
+    //endregion
 
     //region Events
 
@@ -27,6 +36,8 @@ class PlansViewModel @ViewModelInject constructor(
 
     val eventTapBack = CommonLiveEvent<Void>()
 
+    val eventShowHud = CommonLiveEvent<Boolean>()
+
     //endregion
 
     //region Actions
@@ -36,21 +47,73 @@ class PlansViewModel @ViewModelInject constructor(
     }
 
     fun doLoadPlans() {
+        viewModelScope.launch {
+            eventShowHud.postValue(true)
+            backendManager.user().let {
+                cacheManager.user()?.let { user ->
+                    userModel = user
+                    (user.subscription != null).let { value ->
+                        eventLoadCanCancel.postValue(value)
+                    }
+                    eventLoadNextBillDate.postValue(user.subscriptionRenewsDateDisplay)
+                }
+                val response = backendManager.plans()
+                if (response.isSuccess) {
+                    try {
+                        val model = PlansResponseModel.parse(response.responseString)
+                        userModel?.subscriptionText.let {
+                            eventLoadPlans.postValue(model.plans(it))
+                        }
+                    } catch (e: Exception) {
+                        LogManager.log(e)
+                    }
+                }
+            }
+            eventShowHud.postValue(false)
+        }
     }
 
-    fun doCallSubscribe() {
+    fun doCallSubscribe(id: String) {
+        viewModelScope.launch {
+            eventShowHud.postValue(true)
+            val response = backendManager.subscriptionAdd(id)
+            if (response.isSuccess) {
+                doLoadPlans()
+            } else {
+                eventLoadError.postValue("There was an error")
+            }
+            eventShowHud.postValue(false)
+        }
     }
 
     fun doTapBack() {
         eventTapBack.call()
     }
 
-    fun doTapUnSubscribe() {
+    fun doTapUnsubscribe() {
+        viewModelScope.launch {
+            eventShowHud.postValue(true)
+            cacheManager.user()?.subscription?.let {
+                backendManager.subscriptionDelete(it).let { response ->
+                    if (response.isSuccess) {
+                        doLoadPlans()
+                    } else {
+                        eventLoadError.postValue("There was no error")
+                    }
+                }
+            }
+            eventShowHud.postValue(false)
+        }
     }
 
     override fun doTapChoosePlan(model: PlanModel) {
         viewModelScope.launch {
             cacheManager.user()?.let { user ->
+                if (user.subscriptionText != "Pay As You Go") {
+                    eventLoadCancelSubscription.postValue(user.subscription)
+                }
+            } ?: run {
+                eventLoadConfirmSubscription.postValue(model)
             }
         }
     }
