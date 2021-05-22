553d62570fdac2ea3b4d0a9cece540dc02d53f9e
Patrick Martin <patrick@sprintfwd.com>
2021-05-22 13:06:43 -0700

    Add first pass at banner model, rework layout_banner_section with figma paddings, and simpler view hierarchy.

---
+++ app/src/main/java/com/trx/consumer/models/common/BannerModel.kt
@@ -0,0 +1,30 @@
+package com.trx.consumer.models.common
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import org.json.JSONObject
+
+@Parcelize
+class BannerModel(
+    var isActive: Boolean,
+    var modalButtonText: String,
+    var modalDescription: String,
+    var modalImageUrl: String,
+    var modalTitle: String,
+    var offerButtonLink: String
+) : Parcelable {
+
+    companion object {
+
+        fun parse(jsonObject: JSONObject): BannerModel {
+            return BannerModel(
+                isActive = jsonObject.optBoolean("isActive"),
+                modalButtonText = jsonObject.optString("modalButtonText"),
+                modalDescription = jsonObject.optString("modalDescription"),
+                modalImageUrl = jsonObject.optString("modalImageUrl"),
+                modalTitle = jsonObject.optString("modalTitle"),
+                offerButtonLink = jsonObject.optString("offerButtonLink")
+            )
+        }
+    }
+}
