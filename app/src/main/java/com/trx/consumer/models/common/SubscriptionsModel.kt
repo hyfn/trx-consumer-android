1c6075b3572713ecab65a0388e0499fc077731c1
Patrick Martin <patrick@sprintfwd.com>
2021-05-22 15:53:59 -0700

    Requested Change: Shortened parsing for free but logging exception in plans.

--- app/src/main/java/com/trx/consumer/models/common/SubscriptionsModel.kt
+++ app/src/main/java/com/trx/consumer/models/common/SubscriptionsModel.kt
@@ -1,6 +1,7 @@
 package com.trx.consumer.models.common
 
 import com.trx.consumer.extensions.map
+import com.trx.consumer.managers.LogManager
 import org.json.JSONObject
 import java.lang.Exception
 
@@ -13,18 +14,15 @@ class SubscriptionsModel(
 
         fun parse(jsonObject: JSONObject): SubscriptionsModel =
             SubscriptionsModel(
-                free = try {
-                    jsonObject.getJSONObject("free").let {
-                        SubscriptionModel.parse(it)
-                    }
-                } catch (e: Exception) {
-                    SubscriptionModel()
-                },
+                free = jsonObject.optJSONObject("free")?.let {
+                    SubscriptionModel.parse(it)
+                } ?: SubscriptionModel(),
                 plans = try {
                     jsonObject.getJSONArray("plans").let { jsonArray ->
                         jsonArray.map { SubscriptionModel.parse(it) }
                     }
                 } catch (e: Exception) {
+                    LogManager.log(e)
                     listOf()
                 }
             )
