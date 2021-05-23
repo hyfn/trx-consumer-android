b05483512a4d471691f86096b00473cf224a41cc
Patrick Martin <patrick@sprintfwd.com>
2021-05-22 23:42:58 -0700

    Inject backendmanager into EmailViewModel. Initial pass at events from iOS. Complete doLoadView.

--- app/src/main/java/com/trx/consumer/screens/email/EmailViewModel.kt
+++ app/src/main/java/com/trx/consumer/screens/email/EmailViewModel.kt
@@ -1,20 +1,35 @@
 package com.trx.consumer.screens.email
 
+import androidx.hilt.lifecycle.ViewModelInject
 import com.trx.consumer.base.BaseViewModel
 import com.trx.consumer.common.CommonLiveEvent
+import com.trx.consumer.managers.BackendManager
 
-class EmailViewModel : BaseViewModel() {
+class EmailViewModel @ViewModelInject constructor(
+    private val backendManager: BackendManager
+) : BaseViewModel() {
 
     var state: EmailViewState = EmailViewState.FORGOT
 
-    val eventLoadView = CommonLiveEvent<EmailViewState>()
+    val eventLoadView = CommonLiveEvent<Void>()
+    val eventLoadState = CommonLiveEvent<EmailViewState>()
+    val eventLoadButton = CommonLiveEvent<Boolean>()
+
     val eventTapBack = CommonLiveEvent<Void>()
+    val eventTapSendEmail = CommonLiveEvent<EmailViewState>()
+    val eventTapReset = CommonLiveEvent<Void>()
+
+    val eventValidateError = CommonLiveEvent<String>()
+    val eventDismissKeyboard = CommonLiveEvent<Void>()
+    val eventShowHud = CommonLiveEvent<Boolean>()
 
     fun doTapBack() {
         eventTapBack.call()
     }
 
     fun doLoadView() {
-        eventLoadView.postValue(state)
+        eventLoadView.call()
+        eventLoadState.postValue(state)
+        eventLoadButton.postValue(false)
     }
 }
