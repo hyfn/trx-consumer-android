f73147660637c5094bf1ce7f73cc16edbc3073b9
Patrick Martin <patrick@sprintfwd.com>
2021-05-22 23:52:24 -0700

    Observe load(view/state/button) and add handler bodies.

--- app/src/main/java/com/trx/consumer/screens/email/EmailFragment.kt
+++ app/src/main/java/com/trx/consumer/screens/email/EmailFragment.kt
@@ -6,6 +6,7 @@ import com.trx.consumer.R
 import com.trx.consumer.base.BaseFragment
 import com.trx.consumer.base.viewBinding
 import com.trx.consumer.databinding.FragmentEmailBinding
+import com.trx.consumer.managers.LogManager
 import com.trx.consumer.managers.NavigationManager
 
 class EmailFragment : BaseFragment(R.layout.fragment_email) {
@@ -17,24 +18,43 @@ class EmailFragment : BaseFragment(R.layout.fragment_email) {
         val state = NavigationManager.shared.params(this) as EmailViewState
         viewModel.state = state
 
+        viewBinding.btnBack.setOnClickListener { viewModel.doTapBack() }
+
         viewModel.apply {
-            eventTapBack.observe(viewLifecycleOwner, handleTapBack)
             eventLoadView.observe(viewLifecycleOwner, handleLoadView)
+            eventLoadState.observe(viewLifecycleOwner, handleLoadState)
+            eventLoadButton.observe(viewLifecycleOwner, handleLoadButton)
+            eventTapBack.observe(viewLifecycleOwner, handleTapBack)
             doLoadView()
         }
+    }
 
-        viewBinding.btnBack.setOnClickListener { viewModel.doTapBack() }
+    private val handleLoadView = Observer<Void> {
+        LogManager.log("handleLoadView")
+        //  Empty on iOS but in code
+    }
+
+    private val handleLoadState = Observer<EmailViewState> { state ->
+        LogManager.log("handleLoadState")
+        viewBinding.apply {
+            imgHeader.setImageResource(state.headerImage)
+            lblHeader.setText(state.headerTitle)
+            txtEmail.setEmailInputViewState(state)
+            lblDescription.setText(state.description)
+            btnSendEmail.text = getString(state.buttonTitle)
+        }
+    }
+
+    private val handleLoadButton = Observer<Boolean> { enabled ->
+        //  eventLoadButton could be removed all together. States not apparent in figma or iOS. 
+        viewBinding.btnSendEmail.apply {
+            isEnabled = enabled
+            bgColor(if (enabled) R.color.black else R.color.greyDark)
+        }
     }
 
     private val handleTapBack = Observer<Void> {
         NavigationManager.shared.dismiss(this)
-    }
-
-    private val handleLoadView = Observer<EmailViewState> {
-        viewBinding.apply {
-            lblTitle.setText(it.title)
-            btnSendEmail.text = getString(it.buttonTitle)
-        }
     }
 
     override fun onBackPressed() {
