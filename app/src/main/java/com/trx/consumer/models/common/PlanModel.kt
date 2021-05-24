c8662241cca4887308e55450ba4e86bf3ba80389
Patrick Martin <patrick@sprintfwd.com>
2021-05-24 14:27:08 -0700

    Add missing "per Month" string.

--- app/src/main/java/com/trx/consumer/models/common/PlanModel.kt
+++ app/src/main/java/com/trx/consumer/models/common/PlanModel.kt
@@ -17,7 +17,7 @@ class PlanModel(
 ) : Parcelable {
 
     val cost: String
-        get() = if (price != 0.0) price.toPrice() else ""
+        get() = if (price != 0.0) "${price.toPrice()} per Month" else ""
 
     val description: String
         get() = valueProps.joinToString(separator = "\n")
