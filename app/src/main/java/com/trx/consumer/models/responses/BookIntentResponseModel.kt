89174500ba345b7b0cd70daab06cfd50cdcd7311
Patrick Martin <patrick@sprintfwd.com>
2021-05-26 18:12:47 -0700

    Requested Change: Reworking BookIntentResponseModel

--- app/src/main/java/com/trx/consumer/models/responses/BookIntentResponseModel.kt
+++ app/src/main/java/com/trx/consumer/models/responses/BookIntentResponseModel.kt
@@ -4,19 +4,19 @@ import com.trx.consumer.extensions.map
 import com.trx.consumer.models.common.InvoiceModel
 import org.json.JSONObject
 
-class BookIntentResponseModel(val invoices: List<InvoiceModel>) {
+class BookIntentResponseModel(private var invoices: List<InvoiceModel> = listOf()) {
 
     val invoiceId: String
         get() = invoices.firstOrNull()?.id ?: ""
 
     companion object {
         fun parse(json: String): BookIntentResponseModel =
-            JSONObject(json).optJSONObject("data")?.let {
+            JSONObject(json).getJSONObject("data").let {
                 it.optJSONArray("invoices").map { invoicesJSONArray ->
                     InvoiceModel.parse(invoicesJSONArray)
                 }.let { invoicesList ->
                     BookIntentResponseModel(invoicesList)
                 }
-            }.let { BookIntentResponseModel(listOf()) }
+            }
     }
 }
