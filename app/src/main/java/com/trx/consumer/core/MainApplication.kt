0429e8768dab3035c899fde54f3941ccb4ccabe7
devendersinghparihar <devender197@gmail.com>
2021-05-26 17:12:47 -0700

    MainApplication updated

--- app/src/main/java/com/trx/consumer/core/MainApplication.kt
+++ app/src/main/java/com/trx/consumer/core/MainApplication.kt
@@ -1,8 +1,10 @@
 package com.trx.consumer.core
 
 import android.app.Application
+import com.trx.consumer.BuildConfig
 import com.trx.consumer.managers.ConfigManager
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltAndroidApp
@@ -13,6 +15,18 @@ class MainApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        instance = this
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+
         configManager.configure(this)
+    }
+
+    companion object {
+        lateinit var instance: MainApplication
+            private set
     }
 }
