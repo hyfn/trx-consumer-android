f253e82096e13355dd11fe88da2c981f68cb27c1
devendersinghparihar <devender197@gmail.com>
2021-05-30 12:18:41 -0700

    Loading screen added and layout style set

---
+++ app/src/main/java/com/trx/consumer/screens/loading/LoadingStateInterface.kt
@@ -0,0 +1,7 @@
+package com.trx.consumer.screens.loading
+
+import com.trx.consumer.screens.loading.LoadingViewState
+
+interface LoadingStateInterface {
+    fun doLoadCurrentState(loadingViewState: LoadingViewState)
+}
