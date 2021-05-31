291b37891867ea91c0fdc5d97eaf23d336d56f58
devendersinghparihar <devender197@gmail.com>
2021-05-31 14:41:39 -0700

    LoadingStateInterface.kt is deleted and string file updated

--- app/src/main/java/com/trx/consumer/screens/loading/LoadingStateInterface.kt
+++
@@ -1,7 +0,0 @@
-package com.trx.consumer.screens.loading
-
-import com.trx.consumer.screens.loading.LoadingViewState
-
-interface LoadingStateInterface {
-    fun doLoadCurrentState(loadingViewState: LoadingViewState)
-}
