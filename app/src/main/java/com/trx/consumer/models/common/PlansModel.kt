16a8cc9c642c82442468ba0f3adbcf656dc2a171
Patrick Martin <patrick@sprintfwd.com>
2021-06-01 22:16:32 -0700

    Remove experimental extension function.

--- app/src/main/java/com/trx/consumer/models/common/PlansModel.kt
+++ app/src/main/java/com/trx/consumer/models/common/PlansModel.kt
@@ -43,14 +43,3 @@ class PlansModel(
         }
     }
 }
-
-inline fun <T> MutableList<T>.mapInPlace(mutator: (T) -> T) {
-    val iterate = this.listIterator()
-    while (iterate.hasNext()) {
-        val oldValue = iterate.next()
-        val newValue = mutator(oldValue)
-        if (newValue !== oldValue) {
-            iterate.set(newValue)
-        }
-    }
-}
