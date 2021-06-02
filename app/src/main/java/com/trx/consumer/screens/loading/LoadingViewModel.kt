15f27a7ebfec4260aaf84ec64797c338ac412c8c
Vivek Vishwanath <vivekv210@gmail.com>
2021-06-02 11:40:31 -0500

    Implement launch auth loading sequence

--- app/src/main/java/com/trx/consumer/screens/loading/LoadingViewModel.kt
+++ app/src/main/java/com/trx/consumer/screens/loading/LoadingViewModel.kt
@@ -1,21 +1,28 @@
 package com.trx.consumer.screens.loading
 
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.viewModelScope
 import com.trx.consumer.base.BaseViewModel
 import com.trx.consumer.common.CommonLiveEvent
 import com.trx.consumer.managers.BackendManager
+import kotlinx.coroutines.launch
 
-class LoadingViewModel @ViewModelInject constructor(private val backendManager: BackendManager) :
-    BaseViewModel() {
+class LoadingViewModel @ViewModelInject constructor(
+    private val backendManager: BackendManager
+) : BaseViewModel() {
 
     //region Objects
-    var state: LoadingViewState? = null
+    lateinit var state: LoadingViewState
 
     //endregion
 
     //region Events
     val eventShowHud = CommonLiveEvent<Boolean>()
     val eventLoadView = CommonLiveEvent<LoadingViewState>()
+
+    val eventLoadAuth = CommonLiveEvent<Void>()
+    val eventLoadAuthSuccess = CommonLiveEvent<Void>()
+    val eventLoadAuthFailure = CommonLiveEvent<String>()
 
     //endregion
 
@@ -24,6 +31,23 @@ class LoadingViewModel @ViewModelInject constructor(private val backendManager: 
         eventShowHud.postValue(true)
         this.state = state
         eventLoadView.postValue(state)
+        when (state) {
+            LoadingViewState.LAUNCH -> eventLoadAuth.call()
+        }
+    }
+
+    fun doLoadAuth() {
+        viewModelScope.launch {
+            eventShowHud.postValue(true)
+            val response = backendManager.auth()
+            eventShowHud.postValue(false)
+            if (response.isSuccess) {
+                eventLoadAuthSuccess.call()
+            } else {
+                backendManager.updateBeforeLogout()
+                eventLoadAuthFailure.postValue(response.errorMessage)
+            }
+        }
     }
 
     //endregion
