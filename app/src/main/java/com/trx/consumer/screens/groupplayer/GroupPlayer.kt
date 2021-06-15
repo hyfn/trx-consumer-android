e42da39191d10b17d89a98c168bd55354a8366de
Vivek Vishwanath <vivekv210@gmail.com>
2021-06-15 13:45:25 -0500

    Create dummy container screens for live sessions

---
+++ app/src/main/java/com/trx/consumer/screens/groupplayer/GroupPlayer.kt
@@ -0,0 +1,13 @@
+package com.trx.consumer.screens.groupplayer
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.trx.consumer.R
+
+class GroupPlayer : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_group_player)
+    }
+}
