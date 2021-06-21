30a6b28dc2cd5f9049292619a2ec3579661bec34
devendersinghparihar <devender197@gmail.com>
2021-06-21 13:51:46 -0700

    BaseViewModelExtension.kt removed

--- app/src/main/java/com/trx/consumer/extensions/BaseViewModelExtension.kt
+++
@@ -1,6 +0,0 @@
-package com.trx.consumer.extensions
-
-import com.trx.consumer.base.BaseViewModel
-
-inline val BaseViewModel.pageTitle: String
-    get() = this.javaClass.simpleName.replace("ViewModel", "")
