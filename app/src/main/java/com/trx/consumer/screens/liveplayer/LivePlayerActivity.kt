76f8496bd28caeb6b90f8a4795de3c65b1164297
Vivek Vishwanath <vivekv210@gmail.com>
2021-06-22 15:21:53 -0500

    WIP

--- app/src/main/java/com/trx/consumer/screens/liveplayer/LivePlayerActivity.kt
+++ app/src/main/java/com/trx/consumer/screens/liveplayer/LivePlayerActivity.kt
@@ -2,7 +2,9 @@ package com.trx.consumer.screens.liveplayer
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.trx.consumer.R
+import kotlinx.coroutines.launch
 
 class LivePlayerActivity : AppCompatActivity() {
 
