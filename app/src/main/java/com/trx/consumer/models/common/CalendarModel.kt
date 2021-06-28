c195581366bc041e80b2b3f1ac86d8e8aaa4e0fa
devendersinghparihar <devender197@gmail.com>
2021-06-28 15:46:29 -0700

    createListOfDays created

--- app/src/main/java/com/trx/consumer/models/common/CalendarModel.kt
+++ app/src/main/java/com/trx/consumer/models/common/CalendarModel.kt
@@ -1,7 +1,9 @@
 package com.trx.consumer.models.common
 
 import com.trx.consumer.extensions.yearMonthDayString
+import com.trx.consumer.managers.LogManager
 import com.trx.consumer.models.states.CalendarViewState
+import java.util.Calendar
 import java.util.Date
 
 class CalendarModel(var state: CalendarViewState = CalendarViewState.DISPLAY) {
@@ -16,5 +18,28 @@ class CalendarModel(var state: CalendarViewState = CalendarViewState.DISPLAY) {
 
     var initialIndexPath: Int? = listDays.indexOfFirst {
         it.date.yearMonthDayString() == initialDate?.yearMonthDayString()
+    }
+
+    fun createListOfDays(
+        startDate: Date = Date(),
+        endDate: Date? = null,
+        numberOfDays: Int = 7,
+        selectedDate: Date? = null,
+        state: CalendarViewState = CalendarViewState.DISPLAY
+    ): List<DaysModel> {
+        val list: List<DaysModel> = listOf()
+        val calendar = Calendar.getInstance()
+        val end = numberOfDays -1
+        endDate?.let { safeEndDate ->
+            calendar.time = startDate
+            while (calendar.time == safeEndDate) {
+               calendar.add(Calendar.DATE, 1)
+               LogManager.log("${calendar.time}")
+            }
+
+
+        }
+
+        return list
     }
 }
